b0667c00f147b0e8d068faa3f3aab92c8efbe4bd
zhangc <zhangc@educ.college-montreal.qc.ca>
2023-09-26 12:04:50 -0400

    TP2

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-m = 11
-while m >= 0:
-    print('' *(m)+ '*' *m)
-    m = m-1
+#import random number
+import random
+n = random.randint(1, 100)
+
+#Regles du jeu
+print("C'est un jeu de devinettes, les regles sont：\n"
+      "1.un nombre entier de 1 à 100\n"
+      "2.Si le nombre deviné est trop grand,s'affichera:Trop grand!\n"
+      "3.Si le nombre deviné est trop petit,s'affichera:Trop petit!\n"
+      "4.Si vous devinez correctement, s'affichera:Bravo!\n")
+
+#Le fonction donne un random nombre, on doit deviner, input int
+while True:
+    nombre = int(input("Entrez un nombre："))
+    if nombre > n:
+        print("Trop grand!")
+    elif nombre < n:
+        print("Trop petit!")
+    else:
+        print("Bravo!")
+        break
