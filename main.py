54d6de2337cc6ddee911e09027368f00f9628b5c
zhangc <zhangc@educ.college-montreal.qc.ca>
2023-10-12 15:45:25 -0400

    tp2

--- main.py
+++ main.py
@@ -2,32 +2,32 @@
 import random
 def guess_number(lower_limit, upper_limit):
     random_number = random.randint (lower_limit, upper_limit)
-    attempts=0
+    attempts = 0
 
-while True:
-    guess= int(input('Guess the number'.format(lower_limit, upper_limit)))
-    attempts += 1
+    while True:
+        guess= int(input('Deviner le nombre etre {} et {}:'.format(lower_limit, upper_limit)))
+        attempts += 1
 
-    if guess <random_number:
-        print("Too low! Try again.")
-        lower_limit = guess + 1
-    elif guess > random_number:
-        print("Too high! Try again.")
-        upper_limit = guess - 1
-    else:
-        print("Congratulations! You guessed the number () correctly in () attempts.".format
-              (random_number, attempts))
-        break
-
-    play_again = input("Do you want to play again? (yes/no): ").lower()
-    if play_again "yes":
+        if guess < random_number:
+            print("Too low! Try again.")
+            lower_limit = guess + 1
+        elif guess > random_number:
+            print("Too high! Try again.")
+            upper_limit = guess - 1
+        else:
+            print("Bravo! Vous avez deviné le nombre {} correctement lors de {} essai".format
+                (random_number, attempts))
+            break
+#
+    play_again = input("Voulez-vous rejouer? (oui/non): ").lower()
+    if play_again=="oui":
         return True
-    return false
+    return False
 
 while True:
     lower_limit = int(input("Enter the lower limit of the range: "))
     upper_limit = int(input("Enter the upper limit of the range: "))
 
-    play again guess_number (lower_limit, upper limit)
-    if not play again:
+    play_again = guess_number (lower_limit, upper_limit)
+    if not play_again:
         break
