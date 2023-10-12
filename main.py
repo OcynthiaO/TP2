86c3c0eb02f831a51d8d3e5c06d0e5572101a65c
zhangc <zhangc@educ.college-montreal.qc.ca>
2023-10-12 16:14:36 -0400

    tp2

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #import random number
 import random
 
-#definir le nombre à deveir
+#definir les bornesnombre à deveir les essai et guess
 def guess_number(borne_minimale, borne_maximale):
     random_number = random.randint (borne_minimale, borne_maximale)
-    attempts = 0
+    essai = 0
 
     while True:
         guess= int(input('Deviner un nombre etre {} et {}:'.format(borne_minimale, borne_maximale)))
-        attempts += 1
+        essai += 1
 
         if guess < random_number:
             print("Mauvais choix, le nombre est trop petit.")
@@ -18,7 +18,7 @@ def guess_number(borne_minimale, borne_maximale):
             borne_maximale = guess - 1
         else:
             print("Bravo! Vous avez deviné le nombre {} correctement lors de {} essai(s)".format
-                (random_number, attempts))
+                (random_number, essai))
             break
 #Rejouer
     play_again = input("Voulez-vous faire une autre partie? (oui/non): ").lower()
@@ -26,6 +26,7 @@ def guess_number(borne_minimale, borne_maximale):
         return True
     return False
 
+#Run code
 while True:
     borne_minimale = int(input("Enter la borne minimale que vous voulez: "))
     borne_maximale = int(input("Enter la borne maximale of vous voulez: "))
