3ce671d48a6bba44e6864affbf06916b0b72487b
zhangc <zhangc@educ.college-montreal.qc.ca>
2023-10-12 15:26:23 -0400

    tp2

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
 #import random number
 import random
+def guess_number(lower_limit, upper_limit):
+    random_number = random.randint (lower_limit, upper_limit)
+    attempts=0
 
-def guess_number(borne_minimale, borne_maximale)
+while True:
+    guess= int(input('Guess the number'.format(lower_limit, upper_limit)))
+    attempts += 1
+
+    if guess <random_number:
+        print("Too low! Try again.")
+        lower_limit = guess + 1
+    elif guess > random_number:
+        print("Too high! Try again.")
+        upper_limit = guess - 1
+    else:
+        print("Congratulations! You guessed the number () correctly in () attempts.".format
+              (random_number, attempts))
+        break
+
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again "yes":
+        return True
+    return false
+
+while True:
+    lower_limit = int(input("Enter the lower limit of the range: "))
+    upper_limit = int(input("Enter the upper limit of the range: "))
+
+    play again guess_number (lower_limit, upper limit)
+    if not play again:
+        break
